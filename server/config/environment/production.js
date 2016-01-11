362eea0e3b2f621d1753a6f10e3b1bb0b45dd70f
Luis Escobedo <lescobedo@gmail.com>
2016-01-11 13:21:58 -0600

    Production variables for seeding data

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -20,5 +20,8 @@ module.exports = {
           process.env.OPENSHIFT_MONGODB_DB_URL +
           process.env.OPENSHIFT_APP_NAME ||
           'mongodb://localhost/deskquiz'
-  }
+  },
+
+    // Seed database on startup
+    seedDB: true
 };
